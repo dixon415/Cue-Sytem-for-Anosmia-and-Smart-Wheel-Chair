9c666aa35a43b5ff06e1efe47ac34cf3e6a041c7
dixon415 <dudedixon99@gmail.com>
2018-09-11 22:50:34 +0530

    Send Gas Sesnor Values to Google Sheets
    Send Gas Sesnor Values to Google Sheets

--- Cue System for Anosmia/ArduinoMKR1000_Master_Gas_Sensors_Relay/ArduinoMKR1000_Master_Gas_Sensors_Relay.ino
+++ Cue System for Anosmia/ArduinoMKR1000_Master_Gas_Sensors_Relay/ArduinoMKR1000_Master_Gas_Sensors_Relay.ino
@@ -45,19 +45,35 @@
 /*Disabling the secure TLS/SSL connection*/
 #define _DISABLE_TLS_
 
+#include <SPI.h>
 #include <WiFi101.h>
 #include <ThingerWifi101.h>
 #include "arduino_secrets.h"
 
+/*Please enter your sensitive data and credential in the Secret tab/arduino_secrets.h*/
+char ssid[] = SECRET_SSID;  // Your network SSID (name)
+char pass[] = SECRET_PASS;  // Your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;           // Your network key Index number (needed only for WEP)
+
+int status = WL_IDLE_STATUS;
+/*Find instructions to obtain the below value here - https://www.element14.com/community/community/design-challenges/designforacause/blog/2018/08/16/cue-system-for-anosmia-and-smart-wheelchair-5-fall-and-collision-detection#jive_content_id_Obtaining_the_Webhook_URL_to_trigger_an_event_from_Arduino_MKR1000  */
+char server[] = "maker.ifttt.com"; 
+
+/* Initialize the Ethernet client library with the IP address and port of the server that you want to connect to (port 80 is default for HTTP):*/
+WiFiClient client;
+
 /*Declaring Variables*/
 int relayState = 0;
+int collisionLED = 1;     //Red LED to Alert user
+int statusLED = 0;        //Green LED for status
 
 /*Create an account in thinger.io and replace username below with that username. 
 Create a new device and replace deviceId, deviceCredential below with the one you had created.*/
 ThingerWifi101 thing(SECRET_USERNAME, SECRET_DEVICEID, SECRET_DEVICECREDENTIAL);
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(collisionLED, OUTPUT);
+  pinMode(statusLED, OUTPUT);
   pinMode(A0, INPUT);
   pinMode(A4, INPUT);
   pinMode(A5, INPUT);
@@ -91,38 +107,180 @@ void setup() {
   thing["MQ-2 (M)"] >> [](pson& out){
       out = analogRead(A5);
   };
+
+  /*Check for the presence of the WiFi shield:*/
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    /*Don't continue, to inidcate code is stuck here, turn on both the LEDs*/
+    digitalWrite(collisionLED, HIGH);
+    digitalWrite(statusLED, HIGH);
+    while (true);
+  }
+
+  /*Attempt to connect to WiFi network:*/
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    digitalWrite(collisionLED, HIGH);
+    Serial.println(ssid);
+    /*Connect to WPA/WPA2 network. Change this line if using open or WEP network:*/
+    status = WiFi.begin(ssid, pass);
+    /*wait 10 seconds for connection:*/
+    delay(10000);
+  }
+
+  Serial.println("Connected to wifi");
+  printWiFiStatus();
+  delay(5);
 }
 
 void loop() {
   thing.handle();
+  sendToGoogleSheetsExtraUnit();
+  sendToGoogleSheetsMainUnit();
+  delay(1000);
 }
 
 void changeRelayState(){
   if(relayState == 1)
   {
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(statusLED, HIGH);
     Serial1.print(1);
   }
   else if (relayState == 2){
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(statusLED, LOW);
     Serial1.print(2);
   }
   else if(relayState == 3)
   {
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(statusLED, HIGH);
     Serial1.print(3);
   }
   else if (relayState == 4){
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(statusLED, LOW);
     Serial1.print(4);
   }
   else  if(relayState == 5)
   {
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(statusLED, HIGH);
     Serial1.print(5);
   }
   else if (relayState == 6){
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(statusLED, LOW);
     Serial1.print(6);
+  }
+}
+
+void printWiFiStatus() {
+  /*Print the SSID of the network you're attached to:*/
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  /*pPrint your WiFi shield's IP address:*/
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  /*Print the received signal strength:*/
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void sendToGoogleSheetsExtraUnit(){
+  int v1 = analogRead(A0);
+  int v2 = analogRead(A6);
+   
+  String df1 = "{\"value1\":";
+  String df2 = "\"value2\":";
+  String df3 = "\"value3\":";
+  String IFTTT_POST_DATA = df1 + "\"" + String(v1) + "\"" + "," + df2 + "\"" + String(v2) + "\"" + "}" ;
+  String IFTTT_POST_DATA_SIZE = String(IFTTT_POST_DATA.length());
+  
+  Serial.println("\nStarting connection to server...");
+  /*If you get a connection, report back via serial:*/
+  if (client.connect(server, 80)) {
+    
+    
+    Serial.println("connected to server");
+    /*Make a HTTP request:
+    Find instructions to obtain the below value here - https://www.element14.com/community/community/design-challenges/designforacause/blog/2018/08/16/cue-system-for-anosmia-and-smart-wheelchair-5-fall-and-collision-detection#jive_content_id_Obtaining_the_Webhook_URL_to_trigger_an_event_from_Arduino_MKR1000 */
+    client.println("POST /trigger/{YOUR_IFTTT_EVENT_NAME}/with/key/{YOUR_IFTTT_KEY} HTTP/1.1");
+    client.println("Host: maker.ifttt.com");
+    client.println("Connection: close");
+    client.println("Content-Type: application/json");
+    client.println("Content-Length: "+IFTTT_POST_DATA_SIZE);
+    client.println();
+    client.println(IFTTT_POST_DATA);
+
+    digitalWrite(statusLED, HIGH);
+  }
+
+  int reqReceived = 1;
+
+  while(reqReceived){
+  
+    while (client.available()) {
+      char c = client.read();
+      Serial.write(c);
+      reqReceived = 0;
+    }
+  
+//    /*If the server's disconnected, stop the client:*/
+//    if (!client.connected()) {
+//      Serial.println();
+//      Serial.println("disconnecting from server.");
+//      client.stop();
+//      digitalWrite(statusLED, LOW);
+//    }
+  }
+}
+
+void sendToGoogleSheetsMainUnit(){
+  int v1 = analogRead(A4);
+  int v2 = analogRead(A5);
+   
+  String df1 = "{\"value1\":";
+  String df2 = "\"value2\":";
+  String df3 = "\"value3\":";
+  String IFTTT_POST_DATA = df1 + "\"" + String(v1) + "\"" + "," + df2 + "\"" + String(v2) + "\"" + "}" ;
+  String IFTTT_POST_DATA_SIZE = String(IFTTT_POST_DATA.length());
+  
+  Serial.println("\nStarting connection to server...");
+  /*If you get a connection, report back via serial:*/
+  if (client.connect(server, 80)) {
+    
+    
+    Serial.println("connected to server");
+    /*Make a HTTP request:
+    Find instructions to obtain the below value here - https://www.element14.com/community/community/design-challenges/designforacause/blog/2018/08/16/cue-system-for-anosmia-and-smart-wheelchair-5-fall-and-collision-detection#jive_content_id_Obtaining_the_Webhook_URL_to_trigger_an_event_from_Arduino_MKR1000 */
+    client.println("POST /trigger/{YOUR_IFTTT_EVENT_NAME}/with/key/{YOUR_IFTTT_KEY} HTTP/1.1");
+    client.println("Host: maker.ifttt.com");
+    client.println("Connection: close");
+    client.println("Content-Type: application/json");
+    client.println("Content-Length: "+IFTTT_POST_DATA_SIZE);
+    client.println();
+    client.println(IFTTT_POST_DATA);
+
+    digitalWrite(statusLED, HIGH);
+  }
+
+  int reqReceived = 1;
+
+  while(reqReceived){
+  
+    while (client.available()) {
+      char c = client.read();
+      Serial.write(c);
+      reqReceived = 0;
+    }
+  
+//    /*If the server's disconnected, stop the client:*/
+//    if (!client.connected()) {
+//      Serial.println();
+//      Serial.println("disconnecting from server.");
+//      client.stop();
+//      digitalWrite(statusLED, LOW);
+//    }
   }
 }
